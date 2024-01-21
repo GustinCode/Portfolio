19d63a3004adf8999da24bdf39dafa46c2ade234
GustinGame <gustavogoncalves3ch@gmail.com>
2024-01-21 20:27:56 -0300

    Adding some things

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
 const HeroSection = () => {
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-12">
-            <div className="col-span-7">
-                <section>
-                    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">GustInGame</h1>
-                    <p className="text-gray-200">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dapibus ante</p>
-                </section>
+        <section>
+            <div className="grid grid-cols-1 lg:grid-cols-12">
+                <div className="col-span-7 place-self-center">
+                    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Gustavo Henrique</h1>
+                    <p className="text-gray-200 text-lg lg:text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dapibus ante</p>
+                </div>
+                <div className="col-span-5">
+                    <Image src="\public\images\logo.svg" alt='logo image' width={300} height={300} />
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
 
