785497124c405ce9a45529c90f09ca24fca68901
GustinGame <gustavogoncalves3ch@gmail.com>
2024-02-05 22:53:56 -0300

    typing animation and some titles

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,5 +1,7 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
     return (
@@ -11,14 +13,36 @@ const HeroSection = () => {
                 <div className="col-span-7 place-self-center text-center sm:text-left">
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
                         Gustavo Henrique<br />
-                        <span className="text-transparent bg-clip-text bg-gradient-to-l  from-cyan-400 from-30% to-blue-700 ">Desenvolvedor front-end{""}</span>
+                        {/* change this ↓ */}
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'front-end Developer',
+                                5000, // wait 1s before replacing "Mice" with "Hamsters"
+                                'Back-end Developer',
+                                1500,
+                                'AI entusiast',
+                                3000,
+                                'University Student',
+                                1000
+                            ]}
+                            wrapper="span"
+                            speed={1}
+                            style={{ fontSize: '1.4em', display: 'inline-block' }}
+                            repeat={Infinity}
+                        />
+                        <span className="text-transparent bg-clip-text bg-gradient-to-l  from-cyan-400 from-30% to-blue-700 ">
+
+                            Desenvolvedor front-end{""}</span>
                         {/* test */}
                     </h1>
                     <p className="text-gray-200 text-base sm:text-lg lg:text-xl mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dapibus ante</p>
                     <div>
                         {/* possible change? bg-gradient-to-r from-cyan-500 to-purple-700 */}
-                        <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-transparent transition hover:bg-gradient-to-bl from-cyan-400 from-30% to-blue-700 hover:text-white hover:border-2  text-white border border-white mt'>Contacts</button>
-                        <button className='px-6 py-3 w-full sm:w-fit rounded-full mt-2 bg-transparent transition hover:bg-gradient-to-bl from-cyan-400 from-30% to-blue-700  hover:text-black'>Download CV</button>
+                        <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-transparent transition hover:bg-gradient-to-bl from-cyan-400 from-30% to-blue-700 hover:text-white  text-white border border-white mt'>Contacts</button>
+                        <button className='px-1 py-1 w-full sm:w-fit rounded-full mt-2 bg-transparent hover:border-0  hover:bg-gradient-to-bl from-cyan-400 from-30% to-blue-700  '>
+                            <span className='block bg-black hover:bg-zinc-950 rounded-full px-5 py-2'>Download CV</span>
+                        </button>
                     </div>
                 </div>
                 {/* separando 5/12 da tela para guardar a imagem */}
