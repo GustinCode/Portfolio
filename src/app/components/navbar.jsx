97bfeb685a8d77baebb72d5169168436f9186ecb
GustinGame <gustavogoncalves3ch@gmail.com>
2024-02-11 23:01:03 -0300

    adding some things + adding the mobile menu

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,25 +1,53 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import NavLink from './NavLink'
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+// need to fix the code don't forget
+const navLinks = [
+    {
+        title: "About",
+        path: "#about"
+    },
+    {
+        title: "Projects",
+        path: "#projects"
+    },
+    {
+        title: "Contact",
+        path: "#contact"
+    }
+]
 
-const navbar1 = () => {
+const navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+
     return (
         // code for a fixed navbar: fixed top-0 z-50 w-full
-        <nav className='flex flex-wrap justify-between mx-auto p-8'>
-            {/* <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16 border-b-2 border-white p-8 py-7"> */}
-            <Link href="/">
-                <Image src="/images\reversed-logo.svg" alt="home logo" width={35} height={35} />
-            </Link>
-            <div className='menu hidden md:block md:w-auto' id='navbar'>
-                <ul>
-                    <li>
-                        <Link href={"#about"} className='block py-2 pl-3 text-white sm:text-xl rounded md:p-0 hover:text-gray-600'>About</Link>
-                    </li>
-                </ul>
+        <nav className='fixed top-0 right-0 left-0 z-10 bg-gray-950 bg-opacity-90'>
+            <div className="flex flex-wrap items-center justify-between px-3">
+                {/* <div className="flex-between w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16 border-b-2 border-white p-8 py-7"> */}
+                <Link href="/">
+                    <Image src="/images\reversed-logo.svg" alt="home logo" width={70} height={70} />
+                </Link>
+                <div className="mobile-menu block md:hidden">
+                    {navbarOpen ? <button><Bars3Icon className='h-5 w-5' /></button> : <button></button>}
+                </div>
+                <div className='menu hidden mb:block md:w-auto' id='navbar'>
+                    <ul className='flex p-4 md:p-0 md:flex-row md:space-x-6 mt-0'>
+                        {
+                            navLinks.map((link, index) => (
+                                <li key={index}>
+                                    <NavLink href={link.path} title={link.title} />
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
             </div>
-            {/* </div> */}
-        </nav>
+        </nav >
     )
 }
 
-export default navbar1
+export default navbar
