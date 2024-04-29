5980cdab97d9b71fb135a076115091b0081c0a6f
Gustavo Henrique <gustavogoncalves3ch@gmail.com>
2024-04-28 22:09:45 -0300

    Update route.js

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -10,7 +10,7 @@ export async function POST(req, res) {
     try {
         const data = await resend.emails.send({
             from: fromEmail,
-            to: ["gustavogoncalves3ch@gmail.com", email],
+            to: [email],
             subject: subject,
             react: (
                 <>
