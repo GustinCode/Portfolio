39f6adae6ff1fe444ca25a834633631a02b96871
GustinGame <gustavogoncalves3ch@gmail.com>
2025-02-02 20:44:20 -0300

    add: speed insights

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import HeroSection from './components/HeroSection'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from './components/Navbar'
 import AboutSection from './components/AboutSection.jsx'
 import ProjectSection from './components/ProjectSection'
 import EmailSection from './components/EmailSection'
 import Footer from './components/Footer'
-import { Analytics } from "@vercel/analytics/react"
 import AchievementSection from './components/AchievementSection'
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
 
     <main className="flex min-h-screen flex-col bg-black " >
       {/* if you want to add a section with a diferent color [#070707] */}
+      <SpeedInsights />
       <Analytics />
       <Navbar />
       <div className="container mt-20 mx-auto px-12 py-4 mb-8">
